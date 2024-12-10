e553e5c7d26dfe112806f9ca830451a644c49b92
pechorka <pechor44@gmail.com>
2024-12-11 01:22:21 +0500

    tidy up

--- game.rs
+++ game.rs
@@ -1,26 +1,47 @@
-// Global static array for pixels
-const WIDTH: usize = 1600;
-const HEIGHT: usize = 900;
-static mut PIXELS: [u8; WIDTH * HEIGHT * 4] = [0; WIDTH * HEIGHT * 4];
+
+const CELL_WIDTH: usize = 10;
+const CELL_HEIGHT: usize = 10;
+
+const WIDTH: usize = CELL_WIDTH * 100;
+const HEIGHT: usize = CELL_HEIGHT * 50;
+const BYTES_PER_PIXEL: usize = 4;
+static mut PIXELS: [u8; WIDTH * HEIGHT * BYTES_PER_PIXEL] = [0; WIDTH * HEIGHT * BYTES_PER_PIXEL];
 static mut COUNTER: f32 = 0.0;
+
+
+struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
+}
+
+const PLAYER1: Color = Color { r: 255, g: 0, b: 0, a: 255 };
+const PLAYER2: Color = Color { r: 0, g: 0, b: 255, a: 255 };
+
+fn write_color(color: Color, idx: usize) {
+    unsafe {
+        PIXELS[idx] = color.r;
+        PIXELS[idx + 1] = color.g;
+        PIXELS[idx + 2] = color.b;
+        PIXELS[idx + 3] = color.a;
+    }
+}
 
 #[no_mangle]
 pub fn update(dt: f32) {
     unsafe {
         COUNTER += dt;
         
-        // Update the checkerboard pattern
         for y in 0..HEIGHT {
             for x in 0..WIDTH {
-                let idx = ((y * WIDTH + x) * 4) as usize;
-                // Create an alternating pattern that changeis with time
-                let is_white = (x / 8 + y / 8 + (COUNTER as usize)) % 2 == 0;
+                let idx = ((y * WIDTH + x) * BYTES_PER_PIXEL) as usize;
                 
-                // Set RGBA values
-                PIXELS[idx] = if is_white { 255 } else { 0 };     // R
-                PIXELS[idx + 1] = if is_white { 255 } else { 0 }; // G
-                PIXELS[idx + 2] = if is_white { 255 } else { 0 }; // B
-                PIXELS[idx + 3] = 255;                            // A (always fully opaque)
+                if x < WIDTH / 2 {
+                    write_color(PLAYER1, idx);
+                } else {
+                    write_color(PLAYER2, idx);
+                }
             }
         }
     }
